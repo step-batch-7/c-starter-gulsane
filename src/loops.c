58b2c9e0686f51c76a87072d0e2569963b89ed38
gulshan kumar <gulhan@123>
2020-03-31 14:29:33 +0530

    Added print_every_nth_number function in loops.c

--- src/loops.c
+++ src/loops.c
@@ -2,6 +2,7 @@
 
 long double find_factorial(long);
 void get_fibonacci_series(long);
+void print_every_nth_number(long, long, long);
 
 long double find_factorial(long number)
 {
@@ -19,14 +20,25 @@ void get_fibonacci_series(long term)
   }
 }
 
+void print_every_nth_number(long first_num, long last_num, long n)
+{
+  for (first_num += n; first_num < last_num; first_num += n)
+  {
+    printf("%li\n", first_num);
+  }
+}
 int main(void)
 {
-  long number;
+  long number1, number2, number3;
   printf("Enter a number to find its factorial: ");
-  scanf("%li", &number);
-  printf("factorial of %li is %Lf\n", number, find_factorial(number));
+  scanf("%li", &number1);
+  printf("factorial of %li is %Lf\n", number1, find_factorial(number1));
 
   printf("Enter a number to get fibonacci series: ");
-  scanf("%li", &number);
-  get_fibonacci_series(number);
+  scanf("%li", &number1);
+  get_fibonacci_series(number1);
+
+  printf("Enter three num as first num, last num and term to print every nth num: ");
+  scanf("%li %li %li", &number1, &number2, &number3);
+  print_every_nth_number(number1, number2, number3);
 }
