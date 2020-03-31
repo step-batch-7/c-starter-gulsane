e329c7a49941900403fc229bb1798c31ccd53c9c
gulshan kumar <gulhan@123>
2020-03-31 11:40:12 +0530

    Added get_fibonacci_series function in loops.c

--- src/loops.c
+++ src/loops.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
-long double find_factorial(long int);
+long double find_factorial(long);
+void get_fibonacci_series(long);
 
 long double find_factorial(long number)
 {
   return number ? number * find_factorial(number - 1) : 1;
+}
+
+void get_fibonacci_series(long term)
+{
+  long current_term = 0, next_term = 1;
+  for (; term > 0; term--)
+  {
+    next_term = current_term + next_term;
+    current_term = next_term - current_term;
+    printf("%li\n", current_term);
+  }
 }
 
 int main(void)
@@ -13,4 +25,8 @@ int main(void)
   printf("Enter a number to find its factorial: ");
   scanf("%li", &number);
   printf("factorial of %li is %Lf\n", number, find_factorial(number));
+
+  printf("Enter a number to get fibonacci series: ");
+  scanf("%li", &number);
+  get_fibonacci_series(number);
 }
